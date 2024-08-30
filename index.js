9373cc277feb4949984b21b6f88d8071d3095d96
endomorphosis <starworks5@gmail.com>
2024-08-29 20:14:43 -0700

    chore: Refactor class name in main.js for consistency

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { ipfsHuggingfaceScraperJs } from 'ipfs_huggingface_scraper/main.js';
-export { ipfsHuggingfaceScraperJs as default } from 'ipfs_huggingface_scraper/main.js';
+export { ipfsHuggingfaceScraperJs } from 'ipfs_huggingface_scraper_js/main.js';
+export { ipfsHuggingfaceScraperJs as default } from 'ipfs_huggingface_scraper_js/main.js';
