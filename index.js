4ce2a18b1c44a8e7281dd6f4479d6a5afcbcbe77
endomorphosis <starworks5@gmail.com>
2024-09-04 17:03:55 -0700

    chore: Update package version to 1.0.20 and add test_ipfs_huggingface_scraper_js import

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ export { ipfsHuggingfaceScraperJs } from './ipfs_huggingface_scraper_js/main.js'
 export { ipfsHuggingfaceScraperJs as default } from './ipfs_huggingface_scraper_js/main.js';
 export  *  from './test/test.js'
 export { test_ipfs_huggingface_scraper_js } from './test/test.js'
+import { test_ipfs_huggingface_scraper_js } from './test/test.js'
 const test = new test_ipfs_huggingface_scraper_js();
 export { test }
